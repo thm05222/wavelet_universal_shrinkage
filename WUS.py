f9cd8bfb727fc2b5da0f296d34f3318f67bde1b0
thm05222 <thm05222@gmail.com>
2023-07-25 12:51:16 +0800

    Revised the WUS.py file as required.

--- WUS.py
+++ WUS.py
@@ -1,7 +1,9 @@
+import pywt
+import numpy as np
 def wavelet_universal_shrinkage(data,name_of_wavelet = 'sym2',decomposition_level = 5,shrinkage_mode = 'soft'):
     coeffs = pywt.wavedec(data,name_of_wavelet,level=decomposition_level)
     mad = np.abs(np.abs(coeffs[-1])-np.abs(coeffs[-1]).mean()).mean()
-    threshold = mad*np.sqrt(2*np.log(df.__len__()))/0.6745
+    threshold = mad*np.sqrt(2*np.log(data.__len__()))/0.6745
     coeffs_thresholded = list()
     for item in coeffs:
         coeffs_thresholded.append(pywt.threshold(item,threshold,mode = shrinkage_mode))
